37da510b0694966213e820e91886ab93b9ed9a15
Home <castrohenrique13899@gmail.com>
2022-11-29 18:33:22 -0300

    (fix) created md-sys-card in home page

---
+++ src/app/shared/md-sys/md-sys-card/md-sys-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MdSysCardComponent } from './md-sys-card.component';
+
+describe('MdSysCardComponent', () => {
+  let component: MdSysCardComponent;
+  let fixture: ComponentFixture<MdSysCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MdSysCardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MdSysCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
