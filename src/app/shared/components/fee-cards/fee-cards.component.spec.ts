8bd72b3bf662033dc728931d6a2dc7ce623b855a
Home <castrohenrique13899@gmail.com>
2023-02-04 10:48:50 -0300

    (feat): tests for fee-cards

--- src/app/shared/components/fee-cards/fee-cards.component.spec.ts
+++ src/app/shared/components/fee-cards/fee-cards.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeeGetAllMock } from '../../classes/mocks/brasil-api/fee/fee-get-all.mock';
 import { MdSysCardModule } from '../../md-sys/md-sys-card/md-sys-card.module';
 import { MdSysSkeletonModule } from '../../md-sys/md-sys-skeleton/md-sys-skeleton.module';
 
@@ -17,10 +18,27 @@ describe('FeeCardsComponent', () => {
 
     fixture = TestBed.createComponent(FeeCardsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display skeleton when fee is empty', () => {
+    fixture.detectChanges();
+    const feeCardsContainer = fixture.debugElement.nativeElement.querySelector('.fee-cards__container');
+    const mdSysSkeleton = feeCardsContainer.querySelectorAll('md-sys-skeleton');
+
+    expect(mdSysSkeleton.length).toBe(3);
+  });
+
+  it('should display fee-cards when receive fee list', () => {
+    component.fee = FeeGetAllMock;
+    fixture.detectChanges();
+
+    const feeCardsContainer = fixture.debugElement.nativeElement.querySelector('.fee-cards__container');
+    const feeCardsContent = feeCardsContainer.querySelectorAll('.fee-cards__card__content');
+
+    expect(feeCardsContent.length).toBe(3);
   });
 });
